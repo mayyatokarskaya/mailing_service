916dfbcfc8b3f1b67f3dd8405bbc61da3a0c142c
Mayya Tokarskaya <mayya.tokarskaya@gmail.com>
2025-07-13 22:07:46 +1000

    set up Celery for automatic sending

--- config/settings.py
+++ config/settings.py
@@ -114,3 +114,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+CELERY_BROKER_URL = 'redis://localhost:6379/0'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
