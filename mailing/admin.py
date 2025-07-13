530c14bf51704d38171f85d9ea70110064ea7012
Mayya Tokarskaya <mayya.tokarskaya@gmail.com>
2025-07-13 21:57:13 +1000

    added the "Send" button to the admin panel

--- mailing/admin.py
+++ mailing/admin.py
@@ -1,5 +1,11 @@
+from django.urls import path
 from django.contrib import admin
+from django.http import HttpResponseRedirect
+from django.utils.html import format_html
+from django.urls import reverse
+
 from .models import Recipient, Message, Mailing, MailingAttempt
+
 
 @admin.register(Recipient)
 class RecipientAdmin(admin.ModelAdmin):
@@ -7,10 +13,12 @@ class RecipientAdmin(admin.ModelAdmin):
     search_fields = ('email', 'full_name')
     list_filter = ('email',)
 
+
 @admin.register(Message)
 class MessageAdmin(admin.ModelAdmin):
     list_display = ('subject', 'body')
     search_fields = ('subject',)
+
 
 class MailingAttemptInline(admin.TabularInline):  # или StackedInline
     model = MailingAttempt
@@ -21,20 +29,46 @@ class MailingAttemptInline(admin.TabularInline):  # или StackedInline
     def has_add_permission(self, request, obj=None):
         return False
 
+
 @admin.register(Mailing)
 class MailingAdmin(admin.ModelAdmin):
-    list_display = ('id', 'start_time', 'end_time', 'status', 'message')
+    list_display = ('id', 'start_time', 'end_time', 'status', 'send_button')
     list_filter = ('status', 'start_time')
-    search_fields = ('message__subject',)
-    filter_horizontal = ('recipients',)
-    inlines = [MailingAttemptInline]
-    actions = ['send_mailings']
+    actions = ['send_selected_mailings']
+    inlines = [MailingAttemptInline]  # Добавляем inline для попыток
 
-    def send_mailings(self, request, queryset):
+    def get_urls(self):
+        urls = super().get_urls()
+        custom_urls = [
+            path(
+                '<path:object_id>/send/',
+                self.admin_site.admin_view(self.send_mailing_view),
+                name='send_mailing'
+            ),
+        ]
+        return custom_urls + urls
+
+    def send_mailing_view(self, request, object_id):
+        mailing = self.get_object(request, object_id)
+        mailing.send_to_recipients()
+        self.message_user(request, f"Рассылка #{mailing.id} отправлена!")
+        return HttpResponseRedirect(reverse('admin:mailing_mailing_changelist'))
+
+    def send_button(self, obj):
+        return format_html(
+            '<a class="button" href="{}">Отправить</a>',
+            reverse('admin:send_mailing', args=[obj.id])
+        )
+
+    send_button.short_description = "Действие"
+
+    def send_selected_mailings(self, request, queryset):
         for mailing in queryset:
             mailing.send_to_recipients()
-        self.message_user(request, "Выбранные рассылки отправлены")
-    send_mailings.short_description = "Отправить выбранные рассылки"
+        self.message_user(request, "Рассылки отправлены!")
+
+    send_selected_mailings.short_description = "Отправить выбранные рассылки"
+
 
 @admin.register(MailingAttempt)
 class MailingAttemptAdmin(admin.ModelAdmin):
--- mailing/views.py
+++ mailing/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import redirect
+from mailing.models import Mailing
+from django.contrib import messages
 
-# Create your views here.
+def send_mailing_view(request, mailing_id):
+    mailing = Mailing.objects.get(id=mailing_id)
+    mailing.send_to_recipients()
+    messages.success(request, f"Рассылка #{mailing_id} отправлена!")
+    return redirect('admin:mailing_mailing_changelist')
