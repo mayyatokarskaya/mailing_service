96d79149feea2b7f347775f258ab975a17dc8ad5
Mayya Tokarskaya <mayya.tokarskaya@gmail.com>
2025-07-13 22:38:02 +1000

    create users startapp

--- config/settings.py
+++ config/settings.py
@@ -116,3 +116,7 @@ EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
 CELERY_BROKER_URL = "redis://localhost:6379/0"
 CELERY_RESULT_BACKEND = "redis://localhost:6379/0"
+
+AUTH_USER_MODEL = 'users.CustomUser'
+LOGIN_REDIRECT_URL = 'dashboard'
+LOGOUT_REDIRECT_URL = 'home'
