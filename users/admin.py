4d9a1661e84e98ae2d867fc10b1f417a2dc66c53
Mayya Tokarskaya <mayya.tokarskaya@gmail.com>
2025-07-17 08:31:41 +1000

    displaying users group in a

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
-# Register your models here.
+
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ('email', 'username', 'is_staff', 'is_active')
+    list_filter = ('is_staff', 'is_active', 'groups')
+    search_fields = ('email', 'username')
+    ordering = ('email',)
+
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('avatar', 'phone_number', 'country')}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {'fields': ('avatar', 'phone_number', 'country')}),
+    )
